c56d12c938d6bc184c80fb230f50fdc7611029cf
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-12-09 08:29:03 -0300

    minor changes

--- extractor.py
+++ extractor.py
@@ -26,22 +26,6 @@ def extract_data_from_csv():
     
     return dates, infos, values
 
-#def check_csv_info_column(infos):
-#    total_lines = len(infos)
-#    lines_with_pf = 0
-#    lines_with_pj = 0
-
-#    for elem in infos:
-#        if 'pf' in elem:
-#            lines_with_pf += 1
-        
-#        if 'pj' in elem:
-#            lines_with_pj += 1
-    
-#    print(f"total_lines: {total_lines}, lines_with_pf : {lines_with_pf}, lines_with_pj: {lines_with_pj}")
-#    print(f"sum of lines with pf and pj: {lines_with_pf + lines_with_pj}")
-#    print(f"lines without classification: {total_lines - (lines_with_pf + lines_with_pj)}")
-
 def get_periodo_pandemico(date):
     year = int(date.split('-')[0])
     if year < 2020:
@@ -83,8 +67,8 @@ def get_credito_contratacao_data_per_state(dates, infos, values):
 def main():
     dates, infos, values = extract_data_from_csv()
     data = get_credito_contratacao_data_per_state(dates, infos, values)
-    print("dados de sp")
-    pprint(data['sp'], indent=2)
+    #print("dados de sp")
+    #pprint(data['sp'], indent=2)
 
     with open('./visualization/v.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
